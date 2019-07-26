a7a399c84dcecc46cffd514eca84ab657bee0d6f
Daniel Tineli <danieltineli.td@gmail.com>
2019-07-26 17:07:08 -0300

    Criando pool mysql

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.set('views', './app/views');
 
 app.use(express.static(path.join(__dirname + '/app', 'public')));
 
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: err.toString() });
+});
+
 app.listen(3000, () => {
     console.log('server up');
 });
