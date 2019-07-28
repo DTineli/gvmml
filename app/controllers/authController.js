fc4a7343e8d2ca88c1f43c0e7acbee6ce5979a99
Tineli <ju_tineli@hotmail.com>
2019-07-28 12:56:45 -0300

    Adicionada a renovação do access_token

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -3,31 +3,53 @@ const config = require('../config/config');
 
 exports.getAuth = (req, res, next) => {
     let code;
+    let user_id;
     if (code = req.query.code) {
         meli.authorize(code, config.redirect_uri, (err, response) => {
             if (err) {
                 console.log('err');
                 return next(err);
             }
-
+            user_id = response.user_id;
             req.connection.query('SELECT * from userml WHERE user_id = ?',
-                [response.user_id],
+                [user_id],
                 (err, user) => {
                     if (err) {
                         return next(err);
                     }
                     if (user.length == 0) {
                         req.connection.query('INSERT INTO userml (user_id, access_token, refresh_token) VALUES (?,?,?)',
-                            [response.user_id,
-                            response.access_token,
-                            response.refresh_token], (err, results) => {
-                                if (err) {
-                                    next(err);
-                                }
-                                res.redirect('/logged/' + response.user_id);
-                            })
+                            [user_id,
+                                response.access_token,
+                                response.refresh_token], (err, results) => {
+                                    if (err) {
+                                        next(err);
+                                    }
+                                    res.redirect('/logged/' + user_id);
+                                })
                     } else {
-                        res.redirect('/logged/' + response.user_id);
+
+                        meli.get('users/me', { access_token: user[0].access_token }, (err, response) => {
+                            if (err) {
+                                next(err);
+                            }
+                            if (response.message === 'invalid_token') {
+                                meli.refreshAccessToken((err, response) => {
+                                    if (err) {
+
+                                        next(err);
+                                    }
+                                    req.connection.query('UPDATE userml SET access_token = ?, refresh_token = ? WHERE user_id = ?',
+                                        [response.access_token, response.refresh_token, user_id], (err, response) => {
+                                            if (err) {
+                                                console.log(err);
+                                                next(err);
+                                            }
+                                        })
+                                })
+                            }
+                        });
+                        res.redirect('/logged/' + user_id);
                     }
 
                 })
