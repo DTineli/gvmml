73a8fd21372feb2f121c81d6ff45ff5e6b9151ac
Daniel Tineli <danieltineli.td@gmail.com>
2019-07-29 15:59:23 -0300

    Terminai a criação de anuncio

--- app/controllers/anuncioController.js
+++ app/controllers/anuncioController.js
@@ -1,12 +1,14 @@
+const meli = require('../config/meli-factory');
+
 exports.GetProduto = (req, res, next) => {
-    res.render('procuraProduto');
+    res.render('procuraProduto', { access_token: req.query.access_token });
 }
 
 exports.getAnuncio = (req, res, next) => {
     const referencia = req.query.referencia;
 
     if (referencia) {
-        req.connection.query('select p.recnum, descricao, descricao_site, t.valor from produto p left join tabpreitem t on p.recnum = t.fkvariacao where referencia = ? and t.fktabela = ?', [referencia, 2],
+        req.connection.query('select p.recnum, descricao, descricao_site, round(t.valor, 2) as valor from produto p left join tabpreitem t on p.recnum = t.fkvariacao where referencia = ? and t.fktabela = ?', [referencia, 2],
             (err, produto) => {
                 if (err) {
                     next(err);
@@ -20,7 +22,8 @@ exports.getAnuncio = (req, res, next) => {
                             titulo: produto[0].descricao,
                             descricao: produto[0].descricao_site,
                             valor: produto[0].valor,
-                            estoque: estoque[0].saldo
+                            estoque: estoque[0].saldo,
+                            access_token: req.query.access_token
                         });
                     })
                 }
@@ -33,5 +36,28 @@ exports.getAnuncio = (req, res, next) => {
 }
 
 exports.postAnuncio = (req, res, next) => {
-    console.log(req.body);
+    meli.post('items', {
+        "title": req.body.titulo_anuncio,
+        "category_id": "MLB3530",
+        "price": req.body.preco,
+        "currency_id": "BRL",
+        "available_quantity": "1",
+        "buying_mode": "buy_it_now",
+        "listing_type_id": "free",
+        "description": { "plain_text": req.body.descricao },
+        "attributes": [{
+            "id": "ITEM_CONDITION", "value_id": "2230284"
+        }],
+        "sale_terms": [{ "id": "WARRANTY_TYPE", "value_id": "2230279" },
+        { "id": "WARRANTY_TIME", "value_name": "90 dias" }],
+        "pictures": [
+            { "source": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjGsIHt0drjAhUVCrkGHQ5_AU0QjRx6BAgBEAU&url=https%3A%2F%2Fwww.editorajuspodivm.com.br%2Fteste&psig=AOvVaw16CaF-k4gTSGrYwmSK5XqI&ust=1564507255157642" }
+        ]
+    }, { "access_token": req.body.access_token }, (err, response) => {
+        if (err) {
+            console.log('ERRR');
+            console.log(err);
+        }
+        res.render('produtoCriado', { link: response.permalink })
+    });
 }
--- app/controllers/loggedController.js
+++ app/controllers/loggedController.js
@@ -1,3 +1,11 @@
-exports.logged = (req, res) => {
-    res.render('loggedPage');
+exports.logged = (req, res, next) => {
+    req.connection.query('SELECT access_token FROM userml WHERE user_id = ?',
+        [
+            req.params.user_id
+        ], (err, access_token) => {
+            if (err) {
+                next(err);
+            }
+            res.render('loggedPage', { access_token: access_token[0].access_token });
+        })
 }
